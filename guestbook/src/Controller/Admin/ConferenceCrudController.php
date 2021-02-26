a7a370889f2ddf775261d9f2c1dbbe7d72f86e59
Fujita Namiko <fujita-namiko@elecs-web.co.jp>
2021-02-26 16:44:22 +0900

    Add AssociationFields in CRUD Controllers

--- guestbook/src/Controller/Admin/ConferenceCrudController.php
+++ guestbook/src/Controller/Admin/ConferenceCrudController.php
@@ -4,6 +4,10 @@ namespace App\Controller\Admin;
 
 use App\Entity\Conference;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class ConferenceCrudController extends AbstractCrudController
 {
@@ -12,14 +16,13 @@ class ConferenceCrudController extends AbstractCrudController
         return Conference::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('city'),
+            TextField::new('year'),
+            BooleanField::new("isInternational"),
+            AssociationField::new("comments"),
         ];
     }
-    */
 }
