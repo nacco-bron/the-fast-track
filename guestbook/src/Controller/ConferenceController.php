0b0f437efab1794ff0a19b270614684ee9915eb7
Fujita Namiko <fujita-namiko@elecs-web.co.jp>
2021-02-26 18:24:47 +0900

    update Homepage and Conference page

--- guestbook/src/Controller/ConferenceController.php
+++ guestbook/src/Controller/ConferenceController.php
@@ -2,27 +2,57 @@
 
 namespace App\Controller;
 
+use App\Entity\Conference;
+use App\Repository\CommentRepository;
+use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class ConferenceController extends AbstractController
 {
-    #[Route('/hello/{name}', name: 'homepage')]
-    public function index(string $name = ''): Response
+    /** @var Environment $twig */
+    private $twig;
+
+    /** @var CommentRepository $commentRepository */
+    private $commentRepository;
+
+    /** @var ConferenceRepository $conferenceRepository */
+    private $conferenceRepository;
+
+    /**
+     * @param Environment $twig
+     * @param CommentRepository $commentRepository
+     * @param ConferenceRepository $conferenceRepository
+     */
+    public function __construct(
+        Environment $twig,
+        CommentRepository $commentRepository,
+        ConferenceRepository $conferenceRepository
+    ) {
+        $this->twig = $twig;
+        $this->commentRepository = $commentRepository;
+        $this->conferenceRepository = $conferenceRepository;
+    }
+
+    #[Route('/', name: 'homepage')]
+    public function index(): Response
     {
-        $greet = '';
-        if ($name) {
-            $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
-        }
-        return new Response(<<<EOF
-        <html>
-         <body>
-         $greet
-         <img src="/images/under-construction.gif" />
-         </body>
-        </html>
-        EOF);
+        return new Response($this->twig->render('conference/index.html.twig', [
+            'conferences' => $this->conferenceRepository->findAll(),
+        ]));
+    }
+
+
+    #[Route('/conference/{id}', name: 'conference', requirements: ['id' => '\d+'])]
+    public function show(Conference $id): Response
+    {
+        return new Response($this->twig->render('conference/show.html.twig', [
+            'conference' => $id,
+            'comments' => $this->commentRepository->findBy(['conference' =>
+            $id], ['createdAt' => 'DESC']),
+        ]));
     }
 }
